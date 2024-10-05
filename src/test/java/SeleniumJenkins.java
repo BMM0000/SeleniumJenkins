0399b36362d0aa3d49f81eb1520900cba6b1c0ad
D.M.Mistry <bhavin.dmist@gmail.com>
2024-10-05 13:18:26 +0530

    Changes in code

--- src/test/java/SeleniumJenkins.java
+++ src/test/java/SeleniumJenkins.java
@@ -3,57 +3,92 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumJenkins {
 
     public static WebDriver driver;
 
-    @Test
-    public void navigateToUrl(){
+    @BeforeMethod
+    public void launchDriver(){
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\D.S.Mistry\\IdeaProjects\\SeleniumWithJenkins\\src\\main\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("http://the-internet.herokuapp.com/login");
-
-        // Using implicit wait
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        // Get the title of the page
-        String actualTitle = driver.getTitle();
-
-        // Define the expected title (you can find the actual title by inspecting the page)
-        String expectedTitle = "The Internet";
-
-        // Verify the title using Assert.assertEquals()
-        Assert.assertEquals(actualTitle, expectedTitle, "Title verified!");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
     }
 
     @Test
-    public void addLoginDetails()
-    {
-        // Enter the username
-        WebElement usernameField = driver.findElement(By.id("username"));
-        usernameField.sendKeys("tomsmith");
+    public void Test1(){
+        driver.get("https://automationtalks.com/");
+        System.out.println("Test1 title is" + driver.getTitle());
+    }
 
-        // Enter the password
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.sendKeys("SuperSecretPassword!");
+    @Test
+    public void Test2(){
+        driver.get("https://automationtalks.com/");
+    }
 
-        // Click the login button
-        WebElement loginButton = driver.findElement(By.cssSelector("button.radius"));
-        loginButton.click();
+    @Test
+    public void Test3(){
+        driver.get("https://automationtalks.com/");
+    }
 
-        // Print success or failure
-        System.out.println("Before checking success message");
-        WebElement successMessage = driver.findElement(By.cssSelector(".flash.success"));
-        System.out.println("Login Successful: " + successMessage.isDisplayed());
-        System.out.println("After checking success message");
+    @AfterMethod
+    public void closeBrowser(){
 
         driver.quit();
     }
+
+//    @Test
+//    public void navigateToUrl(){
+//
+//        System.setProperty("webdriver.chrome.driver","C:\\Users\\D.S.Mistry\\IdeaProjects\\SeleniumWithJenkins\\src\\main\\resources\\chromedriver.exe");
+//        driver = new ChromeDriver();
+//        driver.get("http://the-internet.herokuapp.com/login");
+//
+//        // Using implicit wait
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//        // Get the title of the page
+//        String actualTitle = driver.getTitle();
+//
+//        // Define the expected title (you can find the actual title by inspecting the page)
+//        String expectedTitle = "The Internet";
+//
+//        // Verify the title using Assert.assertEquals()
+//        Assert.assertEquals(actualTitle, expectedTitle, "Title verified!");
+//
+//    }
+//
+//    @Test
+//    public void addLoginDetails()
+//    {
+//        // Enter the username
+//        WebElement usernameField = driver.findElement(By.id("username"));
+//        usernameField.sendKeys("tomsmith");
+//
+//        // Enter the password
+//        WebElement passwordField = driver.findElement(By.id("password"));
+//        passwordField.sendKeys("SuperSecretPassword!");
+//
+//        // Click the login button
+//        WebElement loginButton = driver.findElement(By.cssSelector("button.radius"));
+//        loginButton.click();
+//
+//        // Print success or failure
+//        System.out.println("Before checking success message");
+//        WebElement successMessage = driver.findElement(By.cssSelector(".flash.success"));
+//        System.out.println("Login Successful: " + successMessage.isDisplayed());
+//        System.out.println("After checking success message");
+//
+//        driver.quit();
+//    }
 }
