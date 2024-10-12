3148d19189bbc2ebe04cbb56056ea071707b4c75
D.M.Mistry <bhavin.dmist@gmail.com>
2024-10-12 18:38:10 +0530

    Code Changes

--- src/test/java/SeleniumJenkins.java
+++ src/test/java/SeleniumJenkins.java
@@ -1,16 +1,10 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
 
 public class SeleniumJenkins {
 
